dbc47bb158a66a0a30dcad7dc3b0202ba8ef5a4e
Nick L. <lesseosnick@gmail.com>
2023-12-23 15:51:59 -0800

    Updated: terraform backend state and added envs to github secrets

---
+++ terraform/droplet.tf
@@ -0,0 +1,24 @@
+resource "digitalocean_droplet" "web_server" {
+  image  = "docker-20-04"  # Use an image with Docker pre-installed
+  name   = "nicklesseos.com"
+  region = var.region
+  size   = "s-1vcpu-1gb"
+
+  # SSH key for accessing the Droplet
+  ssh_keys = [var.ssh_fingerprint]
+
+  # Provisivar.regioncker image
+  provisioner "remote-exec" {
+    inline = [
+      "docker pull ${var.docker_image}",
+      "docker run -d -p 80:3000 ${var.docker_image}"
+    ]
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "root"
+    private_key = var.pvt_key
+    host        = self.ipv4_address
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,33 @@
+variable "do_token" {}
+variable "ssh_fingerprint" {
+    description = "The SSH fingerprint of your public key"
+}
+variable "pvt_key" {
+    description = "The path to your private key"
+}
+variable "docker_image" {
+  description = "The Docker image to deploy"
+  default = "blackflame007/nicklesseos.com:latest"
+}
+
+variable "region" {
+  description = "The region to deploy to"
+  default = "sfo3"
+  
+}
+
+variable "do_space_name" {
+    description = "The name of your DigitalOcean Space"
+    default = "tfstate-echobase"
+}
+
+
+variable "do_spaces_access_key" {
+  description = "DigitalOcean Spaces Access Key"
+  type        = string
+}
+
+variable "do_spaces_secret_key" {
+  description = "DigitalOcean Spaces Secret Key"
+  type        = string
+}
