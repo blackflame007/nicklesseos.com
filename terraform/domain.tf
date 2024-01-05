affa02ce409d8a1920f9ffe7dd7bc58fd9df79c4
Nick L. <lesseosnick@gmail.com>
2024-01-05 14:04:37 -0800

    Added: CNAME to dns tf

--- terraform/domain.tf
+++ terraform/domain.tf
@@ -2,3 +2,11 @@
 resource "digitalocean_domain" "default" {
   name = "nicklesseos.com"
 }
+
+# Create a CNAME record for the domain
+resource "digitalocean_record" "www" {
+  domain = digitalocean_domain.default.name
+  type   = "CNAME"
+  name   = "www"
+  value  = digitalocean_app.nicklesseos-com.live_url
+}
